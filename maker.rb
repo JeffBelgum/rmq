c3ece6a5fefce47663088144c6df5998cf52a48a
mikep <mikep@efi.com>
2014-01-18 09:46:05 -0800

    notes

--- maker.rb
+++ maker.rb
@@ -15,6 +15,10 @@ exchange = channel.topic topic, durable: true
 
 msg = ARGV.empty? ? "here's ya task!" : ARGV.join(" ")
 
+# consider :mandatory
+# condider :type to differentiate "do something" from "C&C:" like "halt" or "restart"
+# consirer :app_id
+# use: :reply_to
 exchange.publish(msg, routing_key: route, persistent: true)
 puts " [x] Sent #{topic} :: #{route} :: #{msg}"
 
