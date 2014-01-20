571fe3af22900b89de0751a3c8a65cc3a518a019
Jeff Belgum <jeffbelgum@gmail.com>
2014-01-20 08:56:51 -0800

    fix typo

--- maker.rb
+++ maker.rb
@@ -17,7 +17,7 @@ msg = ARGV.empty? ? "here's ya task!" : ARGV.join(" ")
 
 # consider :mandatory
 # condider :type to differentiate "do something" from "C&C:" like "halt" or "restart"
-# consirer :app_id
+# consider :app_id
 # use: :reply_to
 exchange.publish(msg, routing_key: route, persistent: true)
 puts " [x] Sent #{topic} :: #{route} :: #{msg}"
